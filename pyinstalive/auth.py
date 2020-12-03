a7051435dafe0f2989987583de2cb90b4f241e01
Michael Zemsky <zmike808@gmail.com>
2020-12-03 12:45:53 +0000

    auth overhaul

--- pyinstalive/auth.py
+++ pyinstalive/auth.py
@@ -18,12 +18,33 @@ except ImportError:
 try:
     from instagram_private_api import (
         Client, ClientError, ClientCheckpointRequiredError, ClientLoginError,
-        ClientCookieExpiredError, ClientLoginRequiredError)
+        ClientCookieExpiredError, ClientLoginRequiredError, ClientSentryBlockError,
+        ClientChallengeRequiredError)
 except ImportError:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     from instagram_private_api import (
         Client, ClientError, ClientCheckpointRequiredError, ClientLoginError,
-        ClientCookieExpiredError, ClientLoginRequiredError)
+        ClientCookieExpiredError, ClientLoginRequiredError, ClientSentryBlockError,
+        ClientChallengeRequiredError)
+
+
+# def to_json(python_object):
+#     if isinstance(python_object, bytes):
+#         return {'__class__': 'bytes',
+#                 '__value__': codecs.encode(python_object, 'base64').decode()}
+#     raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+
+# def from_json(json_object):
+#     if '__class__' in json_object and json_object.get('__class__') == 'bytes':
+#         return codecs.decode(json_object.get('__value__').encode(), 'base64')
+#     return json_object
+
+import json
+import codecs
+import re
+import email
+import imaplib
 
 
 def to_json(python_object):
@@ -34,8 +55,8 @@ def to_json(python_object):
 
 
 def from_json(json_object):
-    if '__class__' in json_object and json_object.get('__class__') == 'bytes':
-        return codecs.decode(json_object.get('__value__').encode(), 'base64')
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
     return json_object
 
 
@@ -46,10 +67,58 @@ def onlogin_callback(api, cookie_file):
         logger.info('New cookie file was made: {0!s}'.format(os.path.basename(cookie_file)))
         logger.separator()
 
+# def onlogin_callback(api, new_settings_file):
+#     cache_settings = api.settings
+#     with open(new_settings_file, 'w') as outfile:
+#         json.dump(cache_settings, outfile, default=to_json)
+#         print('SAVED: {0!s}'.format(new_settings_file))
 
-def login(ig_api: Client):
+
+username = ''
+password = ''
+settings_file = '/path/to/file/settings_{}.json'.format(username)
+
+CHALLENGE_EMAIL = ''
+CHALLENGE_PASSWORD = ''
+
+def get_code_from_email(username):
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(CHALLENGE_EMAIL, CHALLENGE_PASSWORD)
+    mail.select("inbox")
+    result, data = mail.search(None, "(UNSEEN)")
+    assert result == "OK", "Error1 during get_code_from_email: %s" % result
+    ids = data.pop().split()
+    for num in reversed(ids):
+        mail.store(num, "+FLAGS", "\\Seen")  # mark as read
+        result, data = mail.fetch(num, "(RFC822)")
+        assert result == "OK", "Error2 during get_code_from_email: %s" % result
+        msg = email.message_from_string(data[0][1].decode())
+        payloads = msg.get_payload()
+        if not isinstance(payloads, list):
+            payloads = [msg]
+        code = None
+        for payload in payloads:
+            body = payload.get_payload(decode=True).decode()
+            if "<div" not in body:
+                continue
+            match = re.search(">([^>]*?({u})[^<]*?)<".format(u=username), body)
+            if not match:
+                continue
+            print("Match from email:", match.group(1))
+            match = re.search(r">(\d{6})<", body)
+            if not match:
+                print('Skip this email, "code" not found')
+                continue
+            code = match.group(1)
+        return code
+    return False
+
+
+api = Client(username, password, on_login=lambda x: onlogin_callback(x, settings_file))
+
+def login(api: Client):
     try:
-        ig_api.login()
+        api.login()
     except ClientCheckpointRequiredError as e:
         challenge_url = e.challenge_url
 
@@ -62,10 +131,14 @@ def login(ig_api: Client):
         account_id = match_dict['account_id']
         identifier = match_dict['identifier']
 
-        res = ig_api.choose_confirm_method(account_id,
-                                           identifier)  # confirm_method param has default value 1, you can pass 0
-        code = input('Enter code from email: ')
-        ig_api.send_challenge(account_id, identifier, code)
+        res = api.choose_confirm_method(account_id, '1')  # confirm_method param has default value 1, you can pass 0
+        magic_code = get_code_from_email(username)
+        if magic_code:
+            code = magic_code
+        else:
+            code = input('Enter code from email: ')
+        api.send_challenge(account_id, identifier, code)
+
 
 
 def authenticate(username, password, force_use_login_args=False):
@@ -101,13 +174,21 @@ def authenticate(username, password, force_use_login_args=False):
                     username, password,
                     settings=cached_settings, proxy=pil.proxy)
 
-            except ClientCookieExpiredError as e:
-                logger.warn('The current cookie file has expired, creating a new one.')
+            except (ClientSentryBlockError, ClientChallengeRequiredError, ClientCookieExpiredError, ClientLoginError, ClientError) as e:
+                logger.separator()
+                logger.warn('Some sort of login exception!')
+                if pil.verbose:
+                    logger.plain(json.dumps(e.error_response))
+                logger.error('Could not login: {:s}'.format(e.error_response))
+                logger.error('{:s}'.format(json.loads(e.error_response).get("message", e.error_response)))
+                logger.error('{:s}'.format(e.error_response))
+                logger.separator()
 
                 ig_api = Client(
                     username, password,
                     on_login=lambda x: onlogin_callback(x, cookie_file), proxy=pil.proxy)
                 login(ig_api)
+                logger.warn('successfully resolved error and logged back in!')
 
     except (ClientLoginError, ClientError) as e:
         logger.separator()
@@ -119,7 +200,7 @@ def authenticate(username, password, force_use_login_args=False):
         logger.separator()
     except Exception as e:
         if pil.verbose:
-            logger.plain(json.dumps(e.error_response))
+            logger.plain(json.dumps(e))
         if str(e).startswith("unsupported pickle protocol"):
             logger.warn("This cookie file is not compatible with Python {}.".format(sys.version.split(' ')[0][0]))
             logger.warn("Please delete your cookie file '{}.json' and try again.".format(username))
